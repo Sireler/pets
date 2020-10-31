589f05da99397d7a1242731a4c30a307208e8cb4
Yaroshevich Vlad <yaroshevich.vlad00@mail.ru>
2020-10-31 16:05:58 +0300

    catalog cards

--- resources/views/card.blade.php
+++ resources/views/card.blade.php
@@ -68,7 +68,7 @@
                                     <li>
                                         <div class="row w-100">
                                             <div class="col-auto">
-                                                Адрес приюта:
+                                                Телефон:
                                             </div>
                                         </div>
                                     </li>
@@ -187,6 +187,38 @@
                     </div>
                 </div>
             </div>
+        </div>
+    </div>
+
+
+
+        <!-- Дополнительная информация -->
+    <div class="row my-4 border-bottom">
+        <div class="col-auto">
+            <ul class="list-unstyled">
+                <li class="pt-1">
+                    <a class="btn-link-mos form-control border-0"  data-toggle="collapse" href="#collapseQ" role="button" aria-expanded="false" aria-controls="collapseSearch">
+                        <div class="row">
+                            <div class="col-auto">
+                                <div class="row align-items-center">
+                                    <h4>Дополнительная информация</h4>
+                                    <i class="fas fa-angle-down pl-1"></i>
+                                </div>
+
+                            </div>
+                        </div>
+                    </a>
+                    <div class="collapse" id="collapseQ">
+                        <div class="card card-body border-right-0 border-left-0 border-top-0" style="border-radius: 0">
+                            <div>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eum
+                                id itaque molestias natus odio quaerat sequi. A ab aspernatur,
+                                distinctio earum eius eum eveniet nulla praesentium repellendus
+                                repudiandae similique sunt!
+                            </div>
+                        </div>
+                    </div>
+                </li>
+            </ul>
         </div>
     </div>
 
